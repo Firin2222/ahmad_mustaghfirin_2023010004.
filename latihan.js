c080c557a986b2785d3d52d4281481ee84c8a782
Ahmad Mustaghfirin <mustaghfirin2045@gmail.com>
2024-03-20 09:29:31 +0700

    comit

---
+++ latihan.js
@@ -0,0 +1,41 @@
+console.log ("Tolong Baca Ini");
+// Jangan baca itu
+console.log ("Ahmad Mustaghfirin");
+console.log ("2023010004");
+// komen diatas
+
+/* let, var, dan const.
+*/
+let buah;
+buah = "Buah Mangga";
+console.log (buah);
+var kendaraan = "motor";
+console.log (kendaraan);
+const z = "100";
+console.log (z);
+
+let a = 15;
+let b = 5;
+console.log (a+b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+console.log (a%b);
+
+let angka = 13;
+console.log (angka++);
+console.log (angka);
+console.log (++angka);
+
+console.log (4n + 5n);
+console.log (4n - 5n);
+console.log (4n * 5n);
+console.log (4n / 5n);
+console.log (4n % 5n);
+
+const benda = '"kamu pilih benda yang mana?" ini atau itu';
+console.log (benda);
+
+let aku = "makan";
+let moreaku = aku + aku ;
+console.log(moreaku);
